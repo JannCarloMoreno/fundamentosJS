df3367d6acee1167cb33eb768c46aa5045ad1e58
Jann <janncarlomoreno@gmail.com>
2019-04-21 09:39:38 -0500

    class 1: Variables

---
+++ clase1/clase1.js
@@ -0,0 +1,8 @@
+//console.log('hola perro')
+
+var nombre = 'Jann', apellido= 'Moreno',edad= 28, peso= 75
+
+edad = '28 de edad'
+
+console.log('hola '+nombre+' '+apellido)
+console.log('Tengo '+edad)
