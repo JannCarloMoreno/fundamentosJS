2fa5e0b8336682c5303f231bf7ecfb42c3ebfa29
Jann <janncarlomoreno@gmail.com>
2019-04-21 17:31:47 -0500

    Class 7: Destructuring Objects + challenge

---
+++ clase7/clase7.js
@@ -0,0 +1,32 @@
+// variable glogal
+var jann = {
+    nombre: 'Jann',
+    apellido: 'Moreno',
+    edad: 21
+}
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Susnisky',
+    edad: 27
+}
+
+function imprimirNombreEnMayusculas(persona){
+    // var nombre= persona.nombre
+    var { nombre }= persona
+    console.log(nombre.toUpperCase())
+}
+
+function imprimirNombreYEdad(persona){
+    var {nombre, edad} = persona
+    console.log(`Hola, mi nombre es ${nombre} y tengo ${edad}`)
+}
+
+//reto
+imprimirNombreYEdad(jann)
+imprimirNombreYEdad(dario)
+imprimirNombreEnMayusculas(jann)
+imprimirNombreEnMayusculas(dario)
+// imprimirNombreEnMayusculas({nombre: 'Pepito'})
+// imprimirNombreEnMayusculas({ })
+// imprimirNombreEnMayusculas({ apellido: "Perez"})
+// imprimirNombreEnMayusculas()
