409eddef6af1f03860c97b6eefaa5a62e736ce3e
Jann <janncarlomoreno@gmail.com>
2019-04-21 16:08:40 -0500

    Class 5: Function scope

---
+++ clase5/clase5.js
@@ -0,0 +1,9 @@
+// variable glogal
+var nombre = 'Jann'
+
+function imprimirNombreEnMayusculas(nombre){
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+
+imprimirNombreEnMayusculas(nombre)
