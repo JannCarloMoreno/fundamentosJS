e67c082f49f445fcbd0cfce10b03c3de464e0d22
Jann <janncarlomoreno@gmail.com>
2019-04-21 14:05:42 -0500

    class 4: Functions

---
+++ clase4/clase4.js
@@ -0,0 +1,11 @@
+var nombre = 'Jann', edad = 21
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e}`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Carlos', 15)
+imprimirEdad(23, 'Carlos')
+imprimirEdad()
