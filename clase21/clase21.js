1bd3a5cd83e1598804231515ebdb43b7b63441db
Jann <janncarlomoreno@gmail.com>
2019-04-26 18:51:21 -0500

    classes in JS (prototype) + challenge

---
+++ clase21/clase21.js
@@ -0,0 +1,20 @@
+function Persona (nombre, apellido, altura){
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+Persona.prototype.saludar = function() {
+  console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+}
+
+// Reto de la funcion soy alto > a 1.8 metros
+Persona.prototype.soyAlto = function () {
+  console.log((this.altura >= 1.8) ? `yo soy alto` : `yo no soy alto`)
+}
+
+var jann= new Persona('Jann', 'Moreno', 1.66)
+var erika = new Persona('Erika', 'Luna', 1.50)
+var arturo = new Persona('Arturo', 'Martinez', 1.85)
+
+jann.soyAlto()
